98b5216503b92e480401d55eb06c42d5c1c5d49f
Steve Kinney <hello@stevekinney.net>
2023-10-26 09:24:15 -0600

    Run more often, run concurrently

--- src/client.ts
+++ src/client.ts
@@ -2,7 +2,7 @@ import { Connection, Client } from '@temporalio/client';
 import { easilyCompleted, longRunning, failing } from './workflows';
 import { nanoid } from 'nanoid';
 
-const connection = await Connection.connect();
+const connection = await Connection.connect({});
 
 const sleep = async <T = unknown>(value: T, ms = 500): Promise<T> => {
   await new Promise((resolve) => setTimeout(resolve, 500));
@@ -15,53 +15,55 @@ const client = new Client({
   connection,
 });
 
-await client.workflow.start(easilyCompleted, {
-  args: [2],
-  taskQueue,
-  workflowId: `Completed-${nanoid()}`,
-});
-
-await client.workflow.start(longRunning, {
-  args: [],
-  taskQueue,
-  workflowId: `Running-${nanoid()}`,
-});
-
-await client.workflow.start(failing, {
-  args: [],
-  taskQueue,
-  workflowId: `Failed-${nanoid()}`,
-});
-
-await client.workflow
-  .start(longRunning, {
-    args: [],
+for (let i = 0; i < 5; ++i) {
+  await client.workflow.start(easilyCompleted, {
+    args: [2],
     taskQueue,
-    workflowId: `Canceled-${nanoid()}`,
-  })
-  .then(sleep)
-  .then((workflow) => workflow.cancel());
-
-await client.workflow
-  .start(longRunning, {
-    args: [],
-    taskQueue,
-    workflowId: `Terminated-${nanoid()}`,
-  })
-  .then(sleep)
-  .then((workflow) => workflow.terminate());
-
-await client.workflow.start(easilyCompleted, {
-  args: [2, 1],
-  taskQueue,
-  workflowId: `ContinuedAsNew-${nanoid()}`,
-});
-
-try {
-  await client.workflow.execute(longRunning, {
-    args: [],
-    taskQueue,
-    workflowId: `TimedOut-${nanoid()}`,
-    workflowExecutionTimeout: '1s',
+    workflowId: `Completed-${nanoid()}`,
   });
-} catch (error) {}
+
+  await client.workflow.start(longRunning, {
+    args: [],
+    taskQueue,
+    workflowId: `Running-${nanoid()}`,
+  });
+
+  await client.workflow.start(failing, {
+    args: [],
+    taskQueue,
+    workflowId: `Failed-${nanoid()}`,
+  });
+
+  await client.workflow
+    .start(longRunning, {
+      args: [],
+      taskQueue,
+      workflowId: `Canceled-${nanoid()}`,
+    })
+    .then(sleep)
+    .then((workflow) => workflow.cancel());
+
+  await client.workflow
+    .start(longRunning, {
+      args: [],
+      taskQueue,
+      workflowId: `Terminated-${nanoid()}`,
+    })
+    .then(sleep)
+    .then((workflow) => workflow.terminate());
+
+  await client.workflow.start(easilyCompleted, {
+    args: [2, 1],
+    taskQueue,
+    workflowId: `ContinuedAsNew-${nanoid()}`,
+  });
+
+  try {
+    await client.workflow.execute(longRunning, {
+      args: [],
+      taskQueue,
+      workflowId: `TimedOut-${nanoid()}`,
+      workflowExecutionTimeout: '1s',
+    });
+  } catch (error) {}
+}
