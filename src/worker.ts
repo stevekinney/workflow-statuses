262dd30721ce0e6af626bed7e289fbafa292ddbd
Steve Kinney <hello@stevekinney.net>
2025-04-18 13:10:48 -0600

    Remove ES Lint

--- src/worker.ts
+++ src/worker.ts
@@ -1,12 +1,24 @@
 import process from 'node:process';
 import { createRequire } from 'module';
-import { Worker } from '@temporalio/worker';
-import * as activities from './activities';
+import { Worker, NativeConnection } from '@temporalio/worker';
+import * as activities from './activities/index.js';
+import 'dotenv/config';
 
 const require = createRequire(import.meta.url);
 const workflowsPath = require.resolve('./workflows');
 
+const connection = await NativeConnection.connect({
+  address: 'us-east-1.aws.api.temporal.io:7233',
+  apiKey: process.env.TEMPORAL_API_KEY,
+});
+
+// Use the appropriate namespace based on connection type
 const worker = await Worker.create({
+  connection,
+  namespace:
+    process.env.TEMPORAL_API_KEY && process.env.TEMPORAL_NAMESPACE
+      ? process.env.TEMPORAL_NAMESPACE
+      : 'default',
   workflowsPath,
   activities,
   taskQueue: 'workflow-statuses',
